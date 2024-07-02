04e894fc59d57878f11b32bbf6f368bdb5416878
A1ishka <alinamakogon123456@gmail.com>
2024-07-02 16:52:59 +0300

    base hilt DI

--- app/src/main/java/com/a1ishka/bankaccount/MainActivity.kt
+++ app/src/main/java/com/a1ishka/bankaccount/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.a1ishka.bankaccount.navigation.NavGraph
 import com.a1ishka.bankaccount.ui.theme.BankAccountTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/a1ishka/bankaccount/di/BankAccountApp.kt
@@ -0,0 +1,7 @@
+package com.a1ishka.bankaccount.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BankAccountApp : Application()
