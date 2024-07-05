c8da9ce6c4968430f183a9a7f0d456827352cd12
A1ishka <alinamakogon123456@gmail.com>
2024-07-05 12:06:23 +0300

    SetCurrentAccount Event

--- app/src/main/java/com/a1ishka/bankaccount/presentation/account/AccountEvent.kt
+++ app/src/main/java/com/a1ishka/bankaccount/presentation/account/AccountEvent.kt
@@ -4,6 +4,7 @@ import com.a1ishka.bankaccount.domain.Account
 
 sealed interface AccountEvent {
     object SaveAccount : AccountEvent
+    data class SetCurrentAccount(val account: Account) : AccountEvent
     data class DeleteAccount(val account: Account) : AccountEvent
     data class SetAccountName(val name: String) : AccountEvent
     data class SetCardNumber(val cardNumber: Long) : AccountEvent
